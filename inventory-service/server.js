b06fe0b7e8533830625d19970a8ed344c05d0263
Badar Wildani <wildanie12@gmail.com>
2022-08-17 21:21:15 +0700

    add: body parser middleware

--- inventory-service/server.js
+++ inventory-service/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 6000
 const serviceName = process.env.APP_SERVICE_NAME || 'default service name #1'
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.use('/', require('./routes/router'))
 
